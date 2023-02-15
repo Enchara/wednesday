0c61f67bbb708657f1c8f3a3fca408f40feb4356
Enchara <ranjithamahesh719@gmail.com>
2023-02-15 11:57:06 +0530

    push

---
+++ hibernet/src/main/java/project/F1project.java
@@ -0,0 +1,9 @@
+package project;
+
+public class F1project {
+public static void main(String[] args) {
+	EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("dev");
+	EntityManager entityManager =entityManagerFactory.create.EntityManager();
+	EntityTransaction entityTransaction=entityManager.get Transaction+
+}
+}
